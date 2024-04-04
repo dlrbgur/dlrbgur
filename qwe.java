029e94d3cf691891c0a2fc29dcfd05e4a3759f3b
dlrbgur <165352141+dlrbgur@users.noreply.github.com>
2024-04-04 23:59:01 +0900

    Add files via upload
    *
    **
    ***
    ****
    피라미드 for함수 사용해서 코딩하기

---
+++ qwe.java
@@ -0,0 +1,15 @@
+package asd;
+import java.util.Scanner;
+
+public class qwe{
+    public static void main(String[] args) {
+
+        for(int i=0;i<10;i++){
+            for(int in=0;in<i+1;in++){
+                System.out.print('*');
+            }
+            System.out.println();
+        }
+
+    }
+}
